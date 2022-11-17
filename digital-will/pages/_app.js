201ab13ac4e3cc2142b2d29e3f09b6d7c0a1af5b
Ummar Ikram <59392080+ummarikram@users.noreply.github.com>
2022-11-17 16:45:32 +0500

    Defined Structure

--- digital-will/pages/_app.js
+++ digital-will/pages/_app.js
@@ -1,6 +1,34 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import NProgress from 'nprogress'
 
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter()
+
+  // page-transition effect
+  useEffect(() => {
+    const handleStart = (url) => {
+      console.log(`Loading: ${url}`)
+      NProgress.start()
+    }
+
+    const handleStop = () => {
+      NProgress.done()
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleStop)
+    router.events.on('routeChangeError', handleStop)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleStop)
+      router.events.off('routeChangeError', handleStop)
+    }
+  }, [router])
+
   return <Component {...pageProps} />
 }
 
