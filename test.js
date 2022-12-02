0355a5ac22387e32346af859533b6de389258f04
Ummar Ikram <59392080+ummarikram@users.noreply.github.com>
2022-12-02 22:09:06 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const core = require('@actions/core');
 
-fs.readFile(`digital-will\\libs\\stacks\\contracts\\digitalWill\\log.txt`, 'utf16le', (err, data) => {
+fs.readFile(`digital-will\libs\stacks\contracts\digitalWill\\log.txt`, 'utf16le', (err, data) => {
   if (err) {
     console.error(err);
     return;
   }
-  
+  console.log(data);
   if (data.indexOf("FAILED") != -1){
     core.setFailed("Test(s) Failed");
   }
