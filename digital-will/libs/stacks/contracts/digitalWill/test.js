2fb3435839bb1de1c6b2883816ead3cc470f61dd
Ummar Ikram <59392080+ummarikram@users.noreply.github.com>
2022-12-03 00:43:40 +0500

    test

--- digital-will/libs/stacks/contracts/digitalWill/test.js
+++ digital-will/libs/stacks/contracts/digitalWill/test.js
@@ -1,13 +1,18 @@
 const fs = require('fs');
 const core = require('@actions/core');
 
-fs.readFile(`log.txt`, 'utf16le', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(data);
-  if (data.indexOf("FAILED") != -1){
-    core.setFailed("Test(s) Failed");
-  }
-});
+// fs.readFile(`log.txt`, 'utf16le', (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log(data);
+//   if (data.indexOf("FAILED") != -1){
+//     core.setFailed("Test(s) Failed");
+//   }
+// });
+
+process.stdin.on("data", data => {
+  data = data.toString().toUpperCase()
+  process.stdout.write(data + "\n")
+})
