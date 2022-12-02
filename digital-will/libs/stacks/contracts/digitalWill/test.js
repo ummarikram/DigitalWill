b24bb1c4f1b2cbecfb39193a8f18a7600a477131
Ummar Ikram <59392080+ummarikram@users.noreply.github.com>
2022-12-02 22:15:31 +0500

    CI

--- digital-will/libs/stacks/contracts/digitalWill/test.js
+++ digital-will/libs/stacks/contracts/digitalWill/test.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const core = require('@actions/core');
 
-fs.readFile(`digital-will\libs\stacks\contracts\digitalWill\\log.txt`, 'utf16le', (err, data) => {
+fs.readFile(`log.txt`, 'utf16le', (err, data) => {
   if (err) {
     console.error(err);
     return;
