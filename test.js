0c3a7c9df4a48b2f1945ce937f24c6e5ed258862
Ummar Ikram <59392080+ummarikram@users.noreply.github.com>
2022-12-02 22:04:22 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const core = require('@actions/core');
 
-fs.readFile(`${__dirname}\\digital-will\\libs\\stacks\\contracts\\digitalWill\\log.txt`, 'utf16le', (err, data) => {
+fs.readFile(`\\digital-will\\libs\\stacks\\contracts\\digitalWill\\log.txt`, 'utf16le', (err, data) => {
   if (err) {
     console.error(err);
     return;
